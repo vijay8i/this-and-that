8f920072d67c8d24d00921169fab85d745afbf2d
Vijay Aswadhati <vijay@m5nv.com>
2025-03-21 00:32:34 -0700

    11-rsc: exploring react-router

---
+++ 11-rsc/rr/addr_book/app/routes.ts
@@ -0,0 +1,13 @@
+import type { RouteConfig } from "@react-router/dev/routes";
+import { route, index, layout } from "@react-router/dev/routes";
+
+export default [
+  layout("layouts/sidebar.tsx", [
+    index("routes/home.tsx"),
+    route("contacts/:contactId", "routes/contact.tsx"),
+    route("contacts/:contactId/edit", "routes/edit-contact.tsx"),
+    route("contacts/:contactId/destroy", "routes/destroy-contact.tsx"),
+
+  ]),
+  route("about", "routes/about.tsx"),
+] satisfies RouteConfig;
---
+++ 11-rsc/rr/addr_book/app/routes/destroy-contact.tsx
@@ -0,0 +1,10 @@
+import { deleteContact } from "../data";
+import { redirect } from "react-router";
+import type { Route } from "./+types/destroy-contact";
+
+export async function action({
+  params,
+}: Route.ActionArgs) {
+  await deleteContact(params.contactId);
+  return redirect("/");
+}
